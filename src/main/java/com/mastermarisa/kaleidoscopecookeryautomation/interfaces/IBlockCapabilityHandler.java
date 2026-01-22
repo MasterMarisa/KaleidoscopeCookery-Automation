d7136187dda4065519d8c90316ce37e2d0d5cac7
摸里傻 <147955526+MasterMarisa@users.noreply.github.com>
2026-01-22 16:42:41 +0800

    bugfix

--- src/main/java/com/mastermarisa/kaleidoscopecookeryautomation/interfaces/IBlockCapabilityHandler.java
+++
@@ -1,9 +0,0 @@
-package com.mastermarisa.kaleidoscopecookeryautomation.interfaces;
-
-import net.minecraft.core.Direction;
-import net.neoforged.neoforge.items.IItemHandler;
-import org.jetbrains.annotations.Nullable;
-
-public interface IBlockCapabilityHandler {
-    public IItemHandler getHandler(@Nullable Direction side);
-}
